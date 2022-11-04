7d2e5371d729ea8188ca635ddd99702d82fede94
vccalvin33 <vc_calvin@yahoo.com>
2022-11-04 17:42:13 +0700

    add login, logout, and functionalities to the app

--- todolist/app/forms.py
+++ todolist/app/forms.py
@@ -1,10 +1,13 @@
 from django import forms 
 from .models import Task
 
+from datetime import date
+
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ('task', 'description', 'deadline',)
         widgets = {
-            'deadline': forms.DateInput(format=('%m/%d/%Y'), attrs={'class':'form-control', 'style':'width:150px;', 'placeholder':'Select a date', 'type':'date'}),
+            'deadline': forms.DateInput(format=('%Y-%m-%d'), 
+            attrs={'class':'form-control', 'style':'width:150px;', 'placeholder':'Select a date', 'type':'date', 'min': date.today()}),
         }
--- todolist/app/views.py
+++ todolist/app/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+
 
 from .forms import TaskForm
 from .models import Task
 
 # Create your views here.
+@login_required(login_url='login')
 def home(request):
 
-    tasks = Task.objects.filter(task_owner = request.user)
+    tasks = Task.objects.filter(task_owner = request.user).order_by('deadline')
 
     if request.method == 'POST':
         form = TaskForm(request.POST)
@@ -27,4 +32,60 @@ def home(request):
 
     return render(request, 'app/home.html', context)
 
+def edit(request, task_id):
+    task = Task.objects.get(id=task_id)
 
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Task Updated!")
+            return redirect('/')
+    else:
+        form = TaskForm(instance=task)
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'app/edit.html', context)
+
+def delete(request, task_id):
+    task = Task.objects.get(id=task_id)
+
+    if request.method == 'POST':
+        task.delete()
+        messages.success(request, "Task Deleted!")
+        return redirect('/')
+
+    return render(request, 'app/delete.html')
+
+def complete(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.is_completed = not task.is_completed
+    task.save()
+    messages.success(request, "Task Completed!")
+    return redirect('/')
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logged In!")
+            return redirect('/')
+        else:
+            messages.error(request, "Invalid Credentials!")
+            print("user not found")
+            return redirect('login')
+    return render(request, 'app/login.html')
+
+def logout_user(request):
+    print('function is called')
+    logout(request)
+    messages.success(request, "Logged Out!")
+    return redirect('login')
